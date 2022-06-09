0bf98aecbc2a7133a5d0d01e7d5750047388dd20
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-06-09 14:07:30 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Banner from "./Components/Banner/Banner";
 import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
     return (
         <div>
             <Navbar></Navbar>
+            <Banner></Banner>
         </div>
     );
 }
---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,37 @@
+import React from "react";
+import profilePic from "../../assets/images/profile-transparent.png";
+import "./Banner.css";
+
+const Banner = () => {
+    return (
+        <div className="max-w-7xl mx-auto md:mt-12 h-screen">
+            <div className="flex flex-col md:flex-row justify-around items-center">
+                <div className="text-center md:text-left h-[85vh] md:h-auto flex justify-center items-center">
+                    <div>
+                        <h2 className="text-6xl">
+                            Hi! I am{" "}
+                            <span className="font-bold text-primary">
+                                Tanim
+                            </span>
+                        </h2>
+                        <h3 className="text-4xl mt-3">
+                            Front-End Web Developer
+                        </h3>
+                        <button className="resume-btn mt-8">
+                            Download Resume
+                        </button>
+                    </div>
+                </div>
+                <div className="flex justify-center">
+                    <img
+                        className="rounded-full w-4/5"
+                        src={profilePic}
+                        alt=""
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
