c829f40abf7182192f55ddd8fafb5ab570ce5cbc
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-06-11 18:42:23 +0600

    shadows added in project details page

--- src/Components/ProjectDetails/ProjectDetails.js
+++ src/Components/ProjectDetails/ProjectDetails.js
@@ -23,25 +23,43 @@ const ProjectDetails = () => {
             >
                 Back to home
             </button>
-            <div className="px-8 py-20">
+            <div className="px-4 py-20">
                 <div>
-                    <h2 className="text-3xl font-bold text-slate-700 mb-3">
-                        {project?.name}
-                    </h2>
-                    <h2 className="text-2xl font-bold mb-1">
-                        Project Overview:{" "}
-                    </h2>
-                    <p className="text-base font-semibold">
-                        {project?.description}
-                    </p>
-                    <h2 className="text-2xl font-bold mb-1 mt-3">
-                        Technologies used:
-                    </h2>
-                    <p className="text-base font-semibold">
-                        {project?.technologies?.join(", ")}.
-                    </p>
-                    <div>
-                        <h2 className="text-2xl font-bold mt-5 mb-4">
+                    <div
+                        style={{
+                            boxShadow:
+                                "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+                        }}
+                        className="p-8 rounded-xl mb-5"
+                    >
+                        <h2 className="text-3xl font-bold text-slate-700 mb-3">
+                            {project?.name}
+                        </h2>
+                        <div>
+                            <h2 className="text-2xl font-bold mb-1">
+                                Project Overview:{" "}
+                            </h2>
+                            <p className="text-base font-semibold">
+                                {project?.description}
+                            </p>
+                        </div>
+                        <div>
+                            <h2 className="text-2xl font-bold mb-1 mt-3">
+                                Technologies used:
+                            </h2>
+                            <p className="text-base font-semibold">
+                                {project?.technologies?.join(", ")}.
+                            </p>
+                        </div>
+                    </div>
+                    <div
+                        style={{
+                            boxShadow:
+                                "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+                        }}
+                        className="p-8 rounded-xl"
+                    >
+                        <h2 className="text-2xl font-bold mb-4">
                             Screenshots:{" "}
                         </h2>
                         <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
